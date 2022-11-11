ca2513635cd79e2542fecfe67874eb00061a9a53
santy188 <tisanberon@gmail.com>
2022-11-11 00:52:50 -0300

    Agregado validacion mail

--- Tp_integrador/js/pages/help.js
+++ Tp_integrador/js/pages/help.js
@@ -1,4 +1,3 @@
-
 (function () {
     // https://dashboard.emailjs.com/admin/account
     emailjs.init('YuWQA21EuYhOPfVb7');
@@ -8,20 +7,37 @@
 window.onload = function () {
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('service_mq7yx4j', 'template_9s2emfo', this)
-            .then(function () {
-                console.log('SUCCESS!');
-            }, function (error) {
-                console.log('FAILED...', error);
-            });
+        if (validarEmail()) {
+            this.contact_number.value = Math.random() * 100000 | 0;
+            emailjs.sendForm('service_mq7yx4j', 'template_9s2emfo', this)
+                .then(function () {
+                    alert('Enviado correctamente! uwu');
+
+                }, function (error) {
+                    alert('Error... unu', error);
+                });
+        }
+        else {
+            alert("Ingrese un mail valido! owo");
+        }
     });
 }
 
-function limpiar(){
+
+function validarEmail() {
+    let email = document.getElementById('email').value;
+    let caracteres = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (caracteres.test(email)) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function limpiar() {
     document.getElementById('user-name').value = "";
     document.getElementById('email').value = "";
     document.getElementById('mensaje').value = "";
+    alert("Se limpiaron los campos de texto! uwu");
 }
